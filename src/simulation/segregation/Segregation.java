bc5e6499b535c6c7af56382483a5914c6e4bb5be
Ankit Kayastha <ankit.kayastha@duke.edu>
2015-09-25 01:45:06 -0400

    Refactored Segregation

--- src/simulation/segregation/Segregation.java
+++ src/simulation/segregation/Segregation.java
@@ -1,54 +1,25 @@
 package simulation.segregation;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+
 
 import data.Stats;
-import javafx.scene.paint.Color;
 import model.Cell;
 import model.Grid;
-import model.NeighborFactory;
 import simulation.Simulation;
-import simulation.spreading_fire.SpreadingFireCell;
 
 public class Segregation extends Simulation {
 	private final int EMPTY = 0; 
-	private final int simNumber;
 	private double threshold;
 	private String thresh = "threshold";
-	private Color[] myColors;
-	private Random myRandom;
-	private String agents = "agents";
 	private final String agent = "agent";
-	private SpreadingFireCell fireCellManager;
+	private SegregationCellCheck cellCheck = new SegregationCellCheck();
 	
 	public Segregation(Stats stats) {
 		super(stats);
 		if (stats.getGlobalChars().keySet().contains(thresh)){
 			threshold = stats.getGlobalChars().get(thresh);
 		}
-		myRandom = new Random(1234);
-		int numAgents = 0;
-		if (stats.getGlobalChars().keySet().contains(agents)){
-			numAgents = stats.getGlobalChars().get(agents);
-		}
-		myColors = new Color[numAgents + 1];
-		addColors(numAgents);
-		fireCellManager = new SpreadingFireCell
 	}
 	
-	private void addColors(int numAgents) {
-		myColors[0] = Color.WHITE;
-		for (int i = 1; i <= numAgents; i++) {
-			double r = myRandom.nextDouble();
-			double g = myRandom.nextDouble();
-			double b = myRandom.nextDouble();
-			double o = myRandom.nextDouble();
-			myColors[i] = new Color(r, g, b, o);
-		}
-	}
-
 	@Override
 	public void update(Grid myGrid, Stats myStats) {
 		Cell[] oldGrid = super.copyGrid(myGrid, myStats);
@@ -56,14 +27,12 @@ public class Segregation extends Simulation {
 		
 		//moves all dissatisfied cells
 		for (int i = 0; i < myStats.getSize(); i++) {
-			if ((oldGrid[i].getChars().get(agent)!=EMPTY) & !isSatisfied(oldGrid, i, myStats, threshold)) {
-				if (hasEmpty(myGridGrid)) {
-					move(myGridGrid, i);
+			if ((oldGrid[i].getChars().get(agent)!=EMPTY) & !cellCheck.isSatisfied(oldGrid, i, myStats, threshold)) {
+				if (cellCheck.hasEmpty(myGridGrid)) {
+					cellCheck.move(myGridGrid, i);
 				}
 			}
 		}
 	}
-
-
 	
-	}
+}
---
+++ src/simulation/segregation/SegregationCellCheck.java
@@ -0,0 +1,57 @@
+package simulation.segregation;
+
+import java.util.*;
+
+
+import data.Stats;
+import model.Cell;
+import model.NeighborFactory;
+
+public class SegregationCellCheck {
+	private Random myRandom = new Random();
+	private final int EMPTY = 0; 
+	private final String agent = "agent";
+	
+	
+	public void move(Cell[] myArr, int index) {
+		Cell moveCell = findEmpty(myArr);
+		int agentType = myArr[index].getChars().get(agent);
+		moveCell.getChars().put(agent, agentType);
+
+		myArr[index].getChars().put(agent, EMPTY);
+	}
+	
+	private Cell findEmpty(Cell[] myArr) {
+		List<Cell> returnCell = new ArrayList<Cell>();
+		for (int i=0; i<myArr.length; i++) {
+			if (myArr[i].getChars().get(agent)==EMPTY) {
+				returnCell.add(myArr[i]);
+			}
+		}
+		return returnCell.get(myRandom.nextInt(returnCell.size()));
+	}
+	
+	public boolean hasEmpty(Cell[] myArr) {
+		for (int i=0; i<myArr.length; i++) {
+			if (myArr[i].getChars().get(agent)==EMPTY) {
+			}
+		}
+		return false;
+	}
+	
+	public boolean isSatisfied(Cell[] myArr, int index, Stats myStats, double threshold) {
+		int numSameNeighbors = 0;
+		NeighborFactory myNeighborFactory = new NeighborFactory(myStats);
+		
+		List<Cell> cellNeighbors = myNeighborFactory.getNeighbors(myArr, index);		
+		int selectedCellState = myArr[index].getChars().get(agent); 
+		
+		for (Cell cell: cellNeighbors) {
+			if (cell.getChars().get(agent) == selectedCellState) {
+				numSameNeighbors++;
+			}
+		}
+		double percentageSame = (double) numSameNeighbors / cellNeighbors.size();
+		return percentageSame >= (threshold / 100.0);
+	}
+}
